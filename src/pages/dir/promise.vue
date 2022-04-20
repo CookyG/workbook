5ea82af4051daf3f4a725f7f9c0cace0485c6f8c
puhao <379360444@qq.com>
2022-04-21 00:50:50 +0800

    feat: add promise page

---
+++ src/pages/dir/promise.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<script lang="ts">
+export default {
+  name: 'Promise',
+  data() {
+    return {
+      msg: 'Promise',
+    }
+  },
+  mounted() {},
+}
+</script>
+
+<template>
+  <h2 text-30px mb-8>
+    promise
+  </h2>
+</template>
+
+<style>
+
+</style>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,61 +1,18 @@
 <script setup lang="ts">
-import { useUserStore } from '~/stores/user'
-
-const user = useUserStore()
-
-const name = $ref(user.savedName)
-
-const router = useRouter()
-
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
-}
-
-const { t } = useI18n()
+const Router = useRouter()
 </script>
 
 <template>
-  <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
-    </div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      :aria-label="t('intro.whats-your-name')"
-      type="text"
-      autocomplete="false"
-      p="x4 y2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        btn m-3 text-sm
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
+  <div w="100%" class="h-[calc(100%-55px)]">
+    <h3 text-30px mb-6>
+      练习册
+    </h3>
+    <div flex justify-center>
+      <ul>
+        <li list-circle cursor-pointer class="hover:text-blue" @click="Router.push('/dir/promise')">
+          手写Promise
+        </li>
+      </ul>
     </div>
   </div>
 </template>
