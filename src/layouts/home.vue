0e65a6a4acf2859036d27ab5446c61f831a389e2
puhao <379360444@qq.com>
2022-04-21 00:50:35 +0800

    chore: remove useless code

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,9 +1,6 @@
 <template>
-  <main class="px-4 p5y-10 text-center text-gray-700 dark:text-gray-200">
-    <RouterView />
+  <main class="w-100% h-100% px-4 py-10 text-center text-gray-700 dark:text-gray-200">
     <Footer />
-    <div class="mt-5 mx-auto text-center opacity-25 text-sm">
-      [Home Layout]
-    </div>
+    <RouterView />
   </main>
 </template>
