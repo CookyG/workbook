dbd8b9439d7aa1fb1b44aa54acf1e44db67e35e2
puhao <379360444@qq.com>
2022-04-20 14:04:19 +0800

    chore: optimize code

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,9 +2,11 @@
 import { useUserStore } from '~/stores/user'
 
 const user = useUserStore()
+
 const name = $ref(user.savedName)
 
 const router = useRouter()
+
 const go = () => {
   if (name)
     router.push(`/hi/${encodeURIComponent(name)}`)
